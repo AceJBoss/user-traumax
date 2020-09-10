0932e15064ff64f21b78e3e4fdb233abc0defe8c
JBoss <abolade.akintomiwa@gmail.com>
2020-09-10 21:03:35 +0100

    Producer setup

--- src/main/java/com/savvybytes/usertraumax/UserTraumaxApplication.java
+++ src/main/java/com/savvybytes/usertraumax/UserTraumaxApplication.java
@@ -3,11 +3,17 @@ package com.savvybytes.usertraumax;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UserTraumaxApplication {
 
+    @Bean
+    public RestTemplate getTemplate(){
+        return new RestTemplate();
+    }
     public static void main(String[] args) {
         SpringApplication.run(UserTraumaxApplication.class, args);
     }
