e928732aade0b568729eba1f7731a616763cf887
JBoss <abolade.akintomiwa@gmail.com>
2020-09-10 20:25:00 +0100

    Application Setup

---
+++ src/main/java/com/savvybytes/usertraumax/UserTraumaxApplication.java
@@ -0,0 +1,13 @@
+package com.savvybytes.usertraumax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTraumaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserTraumaxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/savvybytes/usertraumax/UserTraumaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savvybytes.usertraumax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTraumaxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
